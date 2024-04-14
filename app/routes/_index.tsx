bdc920175f4a556e181c84f593d400a87be0e957
Lrmdom <lmatiasdomingos@gmail.com>
2024-04-14 21:22:49 +0100

    first commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,32 +10,9 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+     <img
+  src="https://storage.googleapis.com/strapi_images_execlog_aluminium/logos_azul_7354a0aae1/logos_azul_7354a0aae1.webp"
+width="5c00" height="300"  alt="Execlog - Digital Transformation Services, Lda"/>
     </div>
   );
 }
