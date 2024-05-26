212e13bc79c578e4ed54d7e48b5867975f262588
Lrmdom <lmatiasdomingos@gmail.com>
2024-05-26 01:16:11 +0100

    first commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,6 +10,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      Execlog
      <img
   src="https://storage.googleapis.com/strapi_images_execlog_aluminium/logos_azul_7354a0aae1/logos_azul_7354a0aae1.webp"
 width="500" height="300"  alt="Execlog - Digital Transformation Services, Lda"/>
